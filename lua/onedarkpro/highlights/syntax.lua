42c61b8386c01f45f48cb4e29567da15286f1c4e
maeumdaerohae <kermani.nathan@gmail.com>
2024-02-10 18:04:01 +0100

    testing change in syntax.lua

--- lua/onedarkpro/highlights/syntax.lua
+++ lua/onedarkpro/highlights/syntax.lua
@@ -8,35 +8,35 @@ function M.groups(theme)
 
     return {
         Comment = { fg = theme.palette.comment, style = config.styles.comments }, -- Comments
-        Constant = { fg = theme.palette.orange, style = config.styles.constants }, -- (preferred) any constant
-        String = { fg = theme.palette.green, style = config.styles.strings }, --   a string constant: "this is a string"
-        Character = { fg = theme.palette.green }, -- a character constant: 'c', '\n'
-        Number = { fg = theme.palette.orange, style = config.styles.numbers }, -- a number constant: 234, 0xff
-        Float = { fg = theme.palette.orange }, -- a floating point constant: 2.3e10
-        Boolean = { fg = theme.palette.orange }, -- a boolean constant: TRUE, false
+        Constant = { fg = theme.palette.white, style = config.styles.constants }, -- (preferred) any constant
+        String = { fg = theme.palette.yellow, style = config.styles.strings }, --   a string constant: "this is a string"
+        Character = { fg = theme.palette.pink }, -- a character constant: 'c', '\n'
+        Number = { fg = theme.palette.blue, style = config.styles.numbers }, -- a number constant: 240, 0xff
+        Float = { fg = theme.palette.blue }, -- a floating point constant: 2.3e10
+        Boolean = { fg = theme.palette.kaki }, -- a boolean constant: TRUE, false
 
-        Identifier = { fg = theme.palette.red, style = config.styles.variables }, -- (preferred) any variable name
-        Function = { fg = theme.palette.blue, style = config.styles.functions }, -- function name (also: methods for classes)
+        Identifier = { fg = theme.palette.white, style = config.styles.variables }, -- (preferred) any variable name
+        Function = { fg = theme.palette.cyan, style = config.styles.functions }, -- function name (also: methods for classes)
 
-        Statement = { fg = theme.palette.purple }, -- (preferred) any statement
-        Conditional = { fg = theme.palette.purple, style = config.styles.conditionals }, --  if, then, else, endif, switch, etc
-        Repeat = { fg = theme.palette.purple }, -- for, do, while, etc
-        Label = { fg = theme.palette.purple }, -- case, default, etc
+        Statement = { fg = theme.palette.red }, -- (preferred) any statement
+        Conditional = { fg = theme.palette.red, style = config.styles.conditionals }, --  if, then, else, endif, switch, etc
+        Repeat = { fg = theme.palette.red }, -- for, do, while, etc
+        Label = { fg = theme.palette.red }, -- case, default, etc
 
-        Operator = { fg = theme.palette.cyan, style = config.styles.operators }, -- "sizeof", "+", "*", etc
-        Keyword = { fg = theme.palette.purple, style = config.styles.keywords }, -- any other keyword
+        Operator = { fg = theme.palette.red, style = config.styles.operators }, -- "sizeof", "+", "*", etc
+        Keyword = { fg = theme.palette.red, style = config.styles.keywords }, -- any other keyword
         Exception = { link = "Keyword" }, -- try, catch, throw
 
         PreProc = { fg = theme.palette.yellow }, -- (preferred) generic Preprocessor
-        Include = { fg = theme.palette.purple }, -- preprocessor #include
-        Define = { fg = theme.palette.purple }, -- preprocessor #define
-        Macro = { fg = theme.palette.blue }, -- same as Define
-        PreCondit = { fg = theme.palette.yellow }, -- preprocessor #if, #else, #endif, etc
+        Include = { fg = theme.palette.orange }, -- preprocessor #include
+        Define = { fg = theme.palette.orange }, -- preprocessor #define
+        Macro = { fg = theme.palette.kaki }, -- same as Define
+        PreCondit = { fg = theme.palette.orange }, -- preprocessor #if, #else, #endif, etc
 
-        Type = { fg = theme.palette.yellow, style = config.styles.types }, -- (preferred) int, long, char, etc
-        StorageClass = { fg = theme.palette.yellow }, -- static, register, volatile, etc
-        Structure = { fg = theme.palette.purple }, -- struct, union, enum, etc
-        Typedef = { fg = theme.palette.purple }, -- A typedef
+        Type = { fg = theme.palette.red, style = config.styles.types }, -- (preferred) int, long, char, etc
+        StorageClass = { fg = theme.palette.red }, -- static, register, volatile, etc
+        Structure = { fg = theme.palette.red }, -- struct, union, enum, etc
+        Typedef = { fg = theme.palette.red }, -- A typedef
 
         Special = { fg = theme.palette.blue }, -- (preferred) any special symbol
         SpecialChar = { fg = theme.palette.orange }, -- special character in a constant
@@ -51,7 +51,7 @@ function M.groups(theme)
         Italic = { italic = true },
 
         Error = { fg = theme.palette.red }, -- (preferred) any erroneous construct
-        Todo = { fg = theme.palette.purple }, -- (preferred) anything that needs extra attention; mostly the keywords TODO FIXME and XXX
+        Todo = { fg = theme.palette.cyan }, -- (preferred) anything that needs extra attention; mostly the keywords TODO FIXME and XXX
 
         qfLineNr = { link = "LineNr" },
         qfFileName = { link = "Directory" },
